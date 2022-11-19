ee5d0780d086837c980b557699ba1293a046dd12
zeeshansalim1234 <zeeshan.chougle@ucalgary.ca>
2022-11-19 00:39:53 -0800

    added category seggregation

--- app.py
+++ app.py
@@ -8,9 +8,27 @@ user_Ref = db.collection('user')
 
 def add():
     try:
-        input = {'Name':'Atharva', 'Recyclable': '3', 'Non-Recyclable-Value': '5', 'Organic': '7', 'Non-Recyclable-No-Value': '6', 'Rebate': '5000'}
-        user_Ref.document(str(8492384932)).set(input)
+        input = {'Name':'Sarthak', 'Non-Recyclable': 0, 'Recyclable-Value': 0, 'Organic': 0, 'Recyclable-No-Value': 0, 'Rebate': 5000}
+        user_Ref.document(str(8972342342)).set(input)
     except Exception as e:
         return f"An Error Occured: {e}"
 
-add()
+def edit(category):
+
+    try:
+        all_users = [doc.to_dict() for doc in user_Ref.stream()]
+        for user in all_users:
+            if 'Name' in user and user['Name']=="Sarthak":
+
+                if category == "non-recyclable":
+                    user_Ref.document(str(8972342342)).update({"Non-Recyclable": firestore.Increment(1)}) 
+                elif category == "recyclable":
+                    user_Ref.document(str(8972342342)).update({"Recyclable-Value": firestore.Increment(1)}) 
+                elif category == "organic": 
+                    user_Ref.document(str(8972342342)).update({"Organic": firestore.Increment(100)}) 
+
+                
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+edit("organic")
